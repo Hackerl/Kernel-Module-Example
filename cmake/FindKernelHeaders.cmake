d00d692c10d5d3533adbc5e71f069cc0df55f988
Hackerl <490021209@qq.com>
2020-06-17 17:08:31 +0800

    Initial commit

---
+++ cmake/FindKernelHeaders.cmake
@@ -0,0 +1,28 @@
+# Find the kernel release
+execute_process(
+        COMMAND uname -r
+        OUTPUT_VARIABLE KERNEL_RELEASE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Find the headers
+find_path(KERNEL_HEADERS_DIR
+        include/linux/user.h
+        PATHS /lib/modules/${KERNEL_RELEASE}/build
+        )
+
+message(STATUS "Kernel release: ${KERNEL_RELEASE}")
+message(STATUS "Kernel headers: ${KERNEL_HEADERS_DIR}")
+
+if (KERNEL_HEADERS_DIR)
+    set(KERNEL_HEADERS_INCLUDE_DIRS
+            ${KERNEL_HEADERS_DIR}/include
+            ${KERNEL_HEADERS_DIR}/arch/x86/include
+            CACHE PATH "Kernel headers include dirs"
+            )
+    set(KERNEL_HEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
+else (KERNEL_HEADERS_DIR)
+    set(KERNEL_HEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
+endif (KERNEL_HEADERS_DIR)
+
+mark_as_advanced(KERNEL_HEADERS_FOUND)
