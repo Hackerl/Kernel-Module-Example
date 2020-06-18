aa4d7948fffed9195eaf2419b072fb3ff7c8f9b7
Hackerl <490021209@qq.com>
2020-06-18 21:24:00 +0800

    Add#kernel source find path

--- cmake/FindKernelHeaders.cmake
+++ cmake/FindKernelHeaders.cmake
@@ -7,22 +7,34 @@ execute_process(
 
 # Find the headers
 find_path(
-        KERNEL_HEADERS_DIR
-        include/linux/user.h
-        PATHS /lib/modules/${KERNEL_RELEASE}/source
+        KERNEL_DIR
+        modules.symbols
+        PATHS /lib/modules/${KERNEL_RELEASE}
 )
 
 execute_process(
-        COMMAND readlink -fn ${KERNEL_HEADERS_DIR}
+        COMMAND readlink -fn ${KERNEL_DIR}/build
         OUTPUT_VARIABLE KERNEL_HEADERS_DIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+execute_process(
+        COMMAND readlink -fn ${KERNEL_DIR}/source
+        OUTPUT_VARIABLE KERNEL_SOURCE_DIR
         OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 message(STATUS "Kernel release: ${KERNEL_RELEASE}")
 message(STATUS "Kernel headers: ${KERNEL_HEADERS_DIR}")
+message(STATUS "Kernel source: ${KERNEL_SOURCE_DIR}")
 
 if (KERNEL_HEADERS_DIR)
-    set(KERNEL_HEADERS_INCLUDE_DIRS ${KERNEL_HEADERS_DIR}/include ${KERNEL_HEADERS_DIR}/arch/x86/include)
+    set(
+            KERNEL_HEADERS_INCLUDE_DIRS
+            ${KERNEL_SOURCE_DIR}/include
+            ${KERNEL_HEADERS_DIR}/include
+            ${KERNEL_HEADERS_DIR}/arch/x86/include
+    )
     set(KERNEL_HEADERS_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
 else (KERNEL_HEADERS_DIR)
     set(KERNEL_HEADERS_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
